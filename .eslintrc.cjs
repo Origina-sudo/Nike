3f1b17b9d2b4d7d574d5548ca5715e3cadc57ec0
Benjamin Arthur <arthurba8585@gmail.com>
2024-07-02 18:11:42 +0000

    Made a few changes to  eslinctrc.cjs and tailwind.config.js

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -12,10 +12,10 @@ module.exports = {
   settings: { react: { version: '18.2' } },
   plugins: ['react-refresh'],
   rules: {
-    'react/jsx-no-target-blank': 'off',
     'react-refresh/only-export-components': [
       'warn',
       { allowConstantExport: true },
     ],
+    "react/prop-types": 0
   },
 }
