92dd07f217374b4b807b3bbc490a79a052758b21
Benjamin Arthur <arthurba8585@gmail.com>
2024-07-03 13:04:38 +0000

    Added a Button Component to the Hero Section

---
+++ src/Components/Button.jsx
@@ -0,0 +1,17 @@
+
+
+const Button = ({ label, iconURL }) => {
+    return (
+        <button className="flex justify-center items-center 
+   gap-2 px-7 py-4 border 
+   font-montserrat text-lg 
+   leading-none bg-coral-red 
+   rounded-full
+    text-white border-coral-red">{label}
+            <img src={iconURL} alt="arrow right icon"
+                className='ml-2 rounded-full
+   w-5 h-5' /></button>
+    )
+}
+
+export default Button
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,8 +1,33 @@
+import Button from "../components/Button"
 
+import {arrowRight} from '../assets/icons'
 
 const Hero = () => {
   return (
-    <div>Test</div>
+    <section id="home"
+      className="w-full flex 
+    xl:flex-row flex-col 
+    justify-center
+     min-h-screen gap-10 
+     max-container
+    ">
+      <div className="relative xl:w-2/5
+       flex flex-col 
+      justify-center items-start
+      w-full max-xl:padding-x pt-28">
+        <p>Popoi's Summer Collection</p>
+        <h1><span>The New Arrival</span>
+          <br />
+          <span>Nike </span>
+           Shoes
+        </h1>
+        <p>Discover Stylish Nike
+           arrivals, quality comfort,
+           and innovation for your
+          active life. </p>
+          <Button label="Shop now" iconURL={arrowRight}/>
+      </div>
+    </section>
   )
 }
 
