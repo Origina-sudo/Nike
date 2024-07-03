1e8ba3b6ac74091146f48f095ffd92c00ab28f76
Benjamin Arthur <arthurba8585@gmail.com>
2024-07-03 12:10:54 +0000

    Done configuring the Nav component and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
- const App =()=>(
+import {
+  CustomerReviews,Footer,Hero,PopularProducts,Services,SpecialOffer,Subscribe,SuperQuality
+} from './sections'
+import Nav from './Components/Nav';
+
+
+const App =()=>(
   <main className="relative">
-    Nav
+  <Nav />
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+     < Hero/>
     </section>
-    <section className="padding">PopularProducts</section>
-    <section className="padding">SuperQuality</section>
-    <section className="padding-x py-10">Services</section>
-    <section className="padding">SpecialOffer</section>
-    <section className="bg-pale-blue padding ">Customer Reviews</section>
-    <section className="padding-x sm:py-32 py-16 w-full">Suscribe</section>
-    <section className="padding-x bg-black padding-t pb-8">Footer</section>
+    <section className="padding">
+      <PopularProducts/></section>
+    <section className="padding">
+      <SuperQuality/></section>
+    <section className="padding-x py-10">
+      <Services/></section>
+    <section className="padding">
+      <SpecialOffer/></section>
+    <section className="bg-pale-blue padding ">
+      <CustomerReviews/></section>
+    <section className="padding-x sm:py-32 py-16 w-full">
+      <Subscribe/></section>
+    <section className="padding-x bg-black padding-t pb-8">
+      <Footer/></section>
 
     </main>
  );
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -2,7 +2,7 @@
 
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <div>Test</div>
   )
 }
 
