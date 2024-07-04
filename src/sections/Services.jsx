13efb10470468b9d1f82c8129fcbe2bf421327c7
Benjamin Arthur <arthurba8585@gmail.com>
2024-07-04 15:56:03 +0000

    Reverse

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,8 +1,14 @@
+import { services } from "../constants";
+import ServiceCard from "../components/ServiceCard";
 
 
 const Services = () => {
   return (
-    <div>Services</div>
+    <section className="max-container flex justify-center flex-wrap gap-9">
+       {services.map((service)=>(
+        <ServiceCard key={service.label} {...service}/>
+       ))}
+    </section>
   )
 }
 
