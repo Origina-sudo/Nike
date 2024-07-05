78d37f17d884a3ddbd63fa43a5c8f33d9176ca92
Benjamin Arthur <arthurba8585@gmail.com>
2024-07-05 12:05:39 +0000

    Completed ServiceCrad component and the service Section

--- src/Components/ServiceCard.jsx
+++ src/Components/ServiceCard.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
 
-const ServiceCard = () => {
+const ServiceCard = ({imgURL,label,subtext}) => {
   return (
-    <div>ServiceCard</div>
+    <div className='flex-1 
+    sm:w-[350px] sm:min-w-[350px]
+     w-full rounded-[20px] shadow-3xl px-10 py-16'>
+<div>
+  <img src={imgURL} alt="label"
+  width={24} height={24} className='w-11 h-11  flex 
+  justify-center
+   items-center bg-coral-red
+   rounded-full' />
+   
+</div>
+<h3 className='mt-5 font-palanquin text-3xl leading-normal font-bold'>
+  {label}
+  <p className='mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray'>{subtext}</p>
+</h3>
+    </div>
   )
 }
 
