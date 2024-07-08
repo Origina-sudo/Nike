b488ebeafdcf22caae363f5afc0777346842c0fa
Benjamin Arthur <arthurba8585@gmail.com>
2024-07-08 13:55:01 +0000

    Suscribe complete

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,6 +1,6 @@
 
  
-const Button = ({ label, iconURL,backgroundColor,textColor,borderColor }) => {
+const Button = ({ label, iconURL,backgroundColor,textColor,borderColor,fullWidth }) => {
     return (
         <button className={`flex justify-center items-center 
    gap-2 px-7 py-4 border 
@@ -9,7 +9,7 @@ const Button = ({ label, iconURL,backgroundColor,textColor,borderColor }) => {
    ${backgroundColor
     ? `${backgroundColor} ${textColor}
     ${borderColor}` : "bg-coral-red text-white border-coral-red"
-   } rounded-full w-full "}`}>{label}
+   } rounded-full ${fullWidth && 'w-full'}"}`}>{label}
         {iconURL &&    <img src={iconURL} alt="arrow right icon"
         className='ml-2 rounded-full
    w-5 h-5' />}</button>
--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,8 +1,19 @@
 
-
+import Button from "../components/Button"
 const Subscribe = () => {
   return (
-    <div>Subscribe</div>
+    <section className="max-container flex justify-between items-center max-lg:flex-col gap-10"
+      id="contact-us">
+      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
+  Sign Up for <span className="text-coral-red">Updates</span> & NewsLetter<span></span>
+</h3>
+<div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+  <input type="text" placeholder="subsscribe@popoi.com" className="input" />
+  <div className="flex max-sm:justify-end items-center max-sm:w-full">
+    <Button label="Sign Up" fullWidth/>
+  </div>
+</div>
+    </section>
   )
 }
 
